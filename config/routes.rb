a8e6d4a346168f9821cfdef1a121875c5bdc06a5
Alicia Stromback <aliciastromback@gmail.com>
2022-11-14 16:19:49 +0100

    Week 6, Day 1, Stupid Coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'questions#ask' #it connects to the methods of the controllers. The local host will automatically be the thing you root it to so you dont have to go to localhost/ask.
   get 'ask', to: 'questions#ask'
-  get 'answer', to: 'questions#answer'
+  get 'answer', to: 'questions#answer' #get answer method from questionscontroller.
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
